207d0c84a51a33745e7deafab872bdb313cf8375
Rabbi Ahamed <rabbiahamed728@gmail.com>
2025-05-04 23:16:11 +0600

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io     = new Server(server, {
         methods: ["GET", "POST"],
     },
 });
-socketService(server)
+socketService(io.of("/chat-socket"));
 
 app.use(cors());
 app.use(express.json());
